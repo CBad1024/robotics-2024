c674d86934b9e0e29a0a9b24f230ecb54e948788
CBad <aether@gmail.com>
2024-01-30 19:14:31 -0500

    Initial Blank Project

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       chaaranathb                                                  #
+# 	Created:      1/30/2024, 7:07:37 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
